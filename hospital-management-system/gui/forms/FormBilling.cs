367130cf5707715004519e606f3facf77b286f20
emeG0d <emeraldconstantino15@gmail.com>
2021-11-20 15:17:19 +0800

    Revision of BIlling form
    Minor changes in billing form

---
+++ hospital-management-system/gui/forms/FormBilling.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hospital_management_system.gui.forms
+{
+    public partial class FormBilling : Form
+    {
+        public FormBilling()
+        {
+            InitializeComponent();
+        }
+
+        private void labChargeTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
